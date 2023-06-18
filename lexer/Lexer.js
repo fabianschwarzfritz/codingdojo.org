ad0d3b93e64ee27213093ac35a345eabefec1252
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2023-06-18 10:52:27 +0000

    Bootstrap lexer

---
+++ lexer/Lexer.js
@@ -0,0 +1,12 @@
+
+console.log("Production program");
+
+class Lexer {
+    parse() {
+        
+    }
+}
+
+module.exports = {
+    Lexer: Lexer
+}
---
+++ lexer/LexerTest.js
@@ -0,0 +1,34 @@
+
+const { Lexer } = require("./Lexer");
+
+let testIndex = 1;
+
+function test(desc, fn) {
+    console.log(desc)
+    result = false;
+    try {
+        result = fn();
+    } catch (err) {
+        console.log("Error running test", desc, ". Failed with", err);
+        result = false;
+    }
+
+    if (result === true) {
+        console.log(`ok ${testIndex++} - ${desc}`);
+    } else {
+        console.log(`not ok ${testIndex++} - ${desc}`);
+    }
+}
+
+test("Parse variable declaration", () => {
+    const input = `
+        let a = 5;
+    `;
+
+    const lexer = new Lexer();
+    lexer.parse(input);
+
+    return false;
+});
+
+
